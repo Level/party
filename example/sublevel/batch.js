daec7b603219d3f695846867c2a98bd06e737b67
James Halliday <mail@substack.net>
2014-04-02 18:41:42 -0700

    0.0.4

--- example/sublevel/batch.js
+++ example/sublevel/batch.js
@@ -1,5 +1,5 @@
-var level = require('../');
-var sublevel = require('level-sub');
+var level = require('../../');
+var sublevel = require('level-sublevel');
 var db = sublevel(level('/tmp/db', { encoding: 'json' }));
 db.sublevel('xxx').batch([
   { type: 'put', key: Math.random(), value: Math.random() },
--- example/sublevel/x.js
+++ example/sublevel/x.js
@@ -1,5 +1,5 @@
-var level = require('../');
-var sublevel = require('level-sub');
+var level = require('../../');
+var sublevel = require('level-sublevel');
 var db = sublevel(level('/tmp/db', { encoding: 'json' }));
 var xxx = db.sublevel('xxx');
 xxx.createReadStream().on('data', function (row) {
