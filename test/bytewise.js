93f13009899cafb36379ba4fab63e4f29a25f18a
James Halliday <mail@substack.net>
2014-09-24 18:29:23 +0300

    read stream tho

--- test/bytewise.js
+++ test/bytewise.js
@@ -8,18 +8,24 @@ var datadir = path.join(tmpdir, 'level-party-' + Math.random());
 
 var lopts = { keyEncoding: bytewise, valueEncoding: 'json' };
 
-test('two handles', function (t) {
-    t.plan(1);
+test('bytewise key encoding', function (t) {
+    t.plan(5);
     var adb = level(datadir, lopts);
     var bdb = level(datadir, lopts);
     var value = Math.floor(Math.random() * 100000);
     
     adb.put([ 'a' ], value, function (err) {
-        if (err) t.fail(err);
+        t.ifError(err);
         var times = 0;
         
         bdb.get([ 'a' ], function (err, x) {
+            t.ifError(err);
             t.equal(x, value);
+        });
+        
+        bdb.createReadStream().on('data', function (row) {
+            t.deepEqual(row.key, [ 'a' ]);
+            t.deepEqual(row.value, value);
         });
     });
     
