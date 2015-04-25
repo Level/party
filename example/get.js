d8c3dbd249c3173f4660f6c794021df275b35633
Mathias Buus <mathiasbuus@gmail.com>
2015-04-25 13:45:49 -0700

    get/put examples prints out who's the leader

--- example/get.js
+++ example/get.js
@@ -1,6 +1,10 @@
 var level = require('../');
 var db = level(__dirname + '/data', { encoding: 'json' });
 
+db.on('leader', function () {
+  console.log('i am the leader now')
+})
+
 setInterval(function () {
     db.get('a', function (err, value) {
         console.log('a=', value);
--- example/put.js
+++ example/put.js
@@ -1,6 +1,10 @@
 var level = require('../');
 var db = level(__dirname + '/data', { encoding: 'json' });
 
+db.on('leader', function () {
+  console.log('i am the leader now')
+})
+
 var n = Math.floor(Math.random() * 100000);
 
 setInterval(function () {
