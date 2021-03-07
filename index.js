0a2a71110999c3d1c50efc9cf1b01033b8bd4f46
Robert Nagy <ronagy@icloud.com>
2021-03-07 10:18:00 +0100

    fixup: readable-stream + pump opts

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const level = require('level')
-const { pipeline: pump } = require('stream')
+const { pipeline } = require('readable-stream')
 const fs = require('fs')
 const net = require('net')
 const path = require('path')
@@ -14,6 +14,7 @@ module.exports = function (dir, opts = {}) {
 
   opts = { retry: true, ...opts }
 
+  const pump = opts.pump || pipeline
   const client = multileveldown.client(opts)
 
   client.open(tryConnect)
